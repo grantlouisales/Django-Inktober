bdb335c291ac4e34cb5b3aa0547c695dc812b321
Grant Ales <grant.ales@yahoo.com>
2021-10-03 16:07:05 -0600

    finished more stuff

--- InktoberSite/admin.py
+++ InktoberSite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ToDoList
 # Register your models here.
+
+# By typing admin/ on your app site it will bring up admin dashboard.
+admin.site.register(ToDoList)
